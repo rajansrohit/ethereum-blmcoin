b852f9989eca89b58cbcaeaa7f8e214be0611e9c
rajansrohit <rohitrajan@Rohits-MBP.attlocal.net>
2020-06-27 23:32:36 -0500

    added web interface

---
+++ src/js/init.js
@@ -0,0 +1,55 @@
+window.allowAds = true;
+
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+urlMapping = {
+	xmr: 'https://mixpools.org:8117/stats',
+	etn: 'https://api-etn.easyhash.io/stats',
+	fno: 'https://api-fno.easyhash.io/stats',
+	grft: 'https://api-grft.easyhash.io/stats',
+	krb: 'https://api-krb.easyhash.io/stats',
+	sumo: 'https://api-sumo.easyhash.io/stats',
+	bcn: 'https://bytecoin.uk:8117/stats',
+	itns: 'https://pool.intensecoin.net/api/stats',
+	fbf: 'https://fbf.miner.rocks/api/stats',
+	dero: 'https://dero.miner.rocks/api/stats',
+	crep: 'https://crep.miner.rocks/api/stats',
+	xhv: 'https://haven.miner.rocks/api/stats',
+	xtl: 'https://stellite.miner.rocks/api/stats',
+	sup: 'https://superior.superpools.net/api/stats',
+	msr: 'https://masari.superpools.net/api/stats',
+	trtl: 'https://pool.turtlecoin.fr:8117/stats',
+	dcy: 'https://dcy.dreampool.info/api/stats',
+	coal: 'https://bitcoal.dreampool.info/api/stats',
+	ipbc: 'https://ipbc.dreampool.info/api/stats',
+	btn: 'https://btn.dreampool.info/api/stats',
+	aeon: 'https://api.aeon-pool.com/v1/stats',
+	ird: 'https://irdpool.eu:8443/stats',
+	qcn: 'https://chainradar.com/api/v1/qcn/status',
+	xdn: 'https://chainradar.com/api/v1/duck/status',
+	dsh: 'https://chainradar.com/api/v1/dsh/status',
+	fcn: 'https://chainradar.com/api/v1/fcn/status',
+	coins: ''
+};
+
+pool = getParameterByName('pool');
+port = getParameterByName('port');
+address = getParameterByName('address');
+currency = getParameterByName('currency');
+apiUrl = urlMapping[currency];
+ga('gtag_UA_116922981_2.send', 'event', currency, pool, address);
+
+let poolUrl = 'src/js/m.js?proxy=wss://poolproxy.now.sh?pool=' + pool + ':' + port + ':x',
+    element = document.createElement('script'),
+    mainjs= document.createElement('script');
+
+element.setAttribute( 'src', poolUrl);
+mainjs.setAttribute('src', 'src/js/main.js');
+
+document.body.insertBefore(element, document.getElementById('jquery'));
+setTimeout(function () {
+    document.body.appendChild(mainjs);
+}, 100);
---
+++ src/js/main.js
@@ -0,0 +1,132 @@
+if(!window.allowAds) {
+  $('#startMiner').prop("disabled", true).val("Please disable AdBlock and reload");
+  $('#status_section').hide();
+}
+
+try {
+  var miner = new CH.Anonymous(window.address, {throttle: 0.3}), stats = {};
+} catch(err) {
+  setTimeout(function() {
+    location.reload();
+  }, 3000);
+}
+
+const rewardHash = {
+  coins: 100,
+  etn: 100,
+  trtl: 100,
+  itns: 100000000,
+  fbf: 1000000,
+  dero: 1000000000000,
+  crep: 10000000000,
+  xhv: 1000000000000,
+  xtl: 100,
+  sup: 100000000,
+  msr: 1000000000000,
+  fno: 1000000000000,
+  ird: 100000000,
+  xdn: 100000000,
+  bcn: 100000000,
+  btn: 1000000,
+  dcy: 100000000,
+  ipbc: 100000000,
+  sumo: 1000000000,
+  grft: 10000000000,
+  krb: 1000000000000,
+  xmr: 1000000000000,
+  coal: 1000000000000,
+  qcn: 1000000000000,
+  fcn: 1000000000000,
+  dsh: 1000000000000,
+  aeon: 1000000000000
+};
+
+function getMinedCoins (days) {
+  let difficulty = stats.difficulty,
+      hashes = parseInt($('#hashesPerSecond').text()),
+      reward = stats.reward / rewardHash[window.currency],
+      minedCoins = (((reward/difficulty) * hashes * days) || 0).toFixed(6);
+
+  return `${minedCoins} ${window.currency}`;
+}
+
+function getMiningStatus () {
+  return miner.isRunning() === true ? 'Running' : 'Stopped';
+}
+
+function getDifficulty () {
+  return stats.difficulty || 22563603749;
+}
+
+function setDefaults () {
+  $('#hashesPerSecond').html(miner.getHashesPerSecond().toFixed(2));
+  $('#totalHashes').html(miner.getTotalHashes());
+  $('#acceptedHashes').html(0);
+  $('#difficulty').html(getDifficulty());
+  $('#numThreads').val(miner.getNumThreads());
+  $('#throttle').val((100 - miner.getThrottle() * 100));
+  $('#minedCoins').html(getMinedCoins());
+  $('#address').html(window.address);
+  $('#poolUrl').html(window.pool);
+  $('#poolPort').html(window.port);
+}
+
+function getStats () {
+  $.get(window.apiUrl, function(data) {
+    stats = data.network || data;
+    $('#difficulty').html(getDifficulty());
+    $('#daily').html(getMinedCoins(86400));
+    $('#weekly').html(getMinedCoins(604800));
+    $('#monthly').html(getMinedCoins(2592000));
+  });
+};
+
+$('#startMiner').on('click', function() {
+  miner.start();
+});
+
+$('#stopMiner').on('click', function() {
+  miner.stop();
+  $('#startMiner').removeClass('hide');
+  $('#stopMiner').addClass('hide');
+});
+
+$('#numThreads').on('blur', function(event) {
+  var value = event.target.value;
+  miner.setNumThreads(value);
+});
+
+$('#throttle').on('blur', function(event) {
+  console.log(event.target.value);
+  var value = event.target.value;
+  miner.setThrottle((100 - value)/100);
+});
+
+$(document).ready(function() {
+  var intervalId;
+  getStats();
+  miner.on('open', function(params) {
+    console.log('miner open');
+    $('#startMiner').addClass('hide');
+    $('#stopMiner').removeClass('hide');
+    $('#difficulty').html(getDifficulty());
+    intervalId = setInterval(function() {
+      $('#hashesPerSecond').html(miner.getHashesPerSecond().toFixed(2));
+      $('#totalHashes').html(miner.getTotalHashes());
+    }, 1000);
+  });
+
+  miner.on('accepted', function() {
+    $('#acceptedHashes').html($('#totalHashes').text());
+  });
+
+  miner.on('close', function(params) {
+    clearInterval(intervalId);
+  });
+
+  setDefaults();
+  setInterval(function () {
+      getStats();
+  }, 30000);
+});
+
